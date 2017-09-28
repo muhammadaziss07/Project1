c8105912d2dff8b2dd3d46411456f6e5f41bf9e1
muhammadaziss07 <muhammadaziss07@users.noreply.github.com>
2017-09-28 17:32:41 +0700

    LEVEL 2 - Query to Update Username and Password

--- login_update.php
+++ login_update.php
@@ -1,6 +1,26 @@
 <?php include "db.php"; ?>
 <?php include "functions.php"; ?>
 
+<?php
+
+if(isset($_POST['submit'])){
+	$id			= $_POST['id'];
+	$username	= $_POST['username'];
+	$password	= $_POST['password'];
+	
+	$query = "UPDATE users
+				SET username = '$username',
+					password = '$password'
+				WHERE id = $id";
+	$result = mysqli_query($koneksi, $query);
+	
+	if(!$result){
+		die("Query Failed.");
+	}
+}
+
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
